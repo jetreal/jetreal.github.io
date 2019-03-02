37cf714c23b55abd45941e01ade80d3ed2f01315
jetreal <bystryi.sokol@gmail.com>
2019-03-03 00:23:55 +0300

    Update common.js

--- nature/js/common.js
+++ nature/js/common.js
@@ -37,36 +37,38 @@ $(window).on('load', function() {
 ////////////////////////////////////////////////////////////////////////
 
 // magnific Popup
-jQuery(document).ready(function(){
-	jQuery('.test-popup-link').magnificPopup({
-		type: 'image',
-		gallery : {
-		enabled : true,
-		closeBtnInside: true,
-		navigateByImgClick: true
-	},
-	image: {
-		titleSrc: 'title'
-	},
-	removalDelay: 300,
-	mainClass: 'mfp-fade mfp-with-zoom'
-	});
-});
-
-$('.popup-modal2').magnificPopup({
-		type: 'image',
-		closeOnContentClick: true,
-		closeBtnInside: true,
-		fixedContentPos: false,
-		mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
-		image: {
-			verticalFit: true
+if ($(window).outerWidth > 768) {
+	jQuery(document).ready(function(){
+		jQuery('.test-popup-link').magnificPopup({
+			type: 'image',
+			gallery : {
+			enabled : true,
+			closeBtnInside: true,
+			navigateByImgClick: true
 		},
-		zoom: {
-			enabled: true,
-			// duration: 300 // don't foget to change the duration also in CSS
-		}
+		image: {
+			titleSrc: 'title'
+		},
+		removalDelay: 300,
+		mainClass: 'mfp-fade mfp-with-zoom'
+		});
 	});
+
+	$('.popup-modal2').magnificPopup({
+			type: 'image',
+			closeOnContentClick: true,
+			closeBtnInside: true,
+			fixedContentPos: false,
+			mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+			image: {
+				verticalFit: true
+			},
+			zoom: {
+				enabled: true,
+				// duration: 300 // don't foget to change the duration also in CSS
+			}
+		});
+}
 /////////////////////////////////////////////////////////////
 // forest section
 
