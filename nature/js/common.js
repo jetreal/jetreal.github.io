d5435c84b1ee8f2b9d050ff75be5be2a0fdfcbe8
jetreal <bystryi.sokol@gmail.com>
2019-03-10 15:52:58 +0300

    fd

--- nature/js/common.js
+++ nature/js/common.js
@@ -2148,3 +2148,9 @@ $(document).ready(function() {
 // 	'img/images/kanari1.webp',
 // 	'img/images/kanari2.webp'
 // ]
+tt:setTimeout(function(){
+if(self!==null){
+self.delaylist[name].tt=0;
+fn.call();
+}
+},tm);
