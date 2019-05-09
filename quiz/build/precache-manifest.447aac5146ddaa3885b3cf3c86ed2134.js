f9e94a332ad3a2b85c959d3cf209edd21ade00a9
jetreal <bystryi.sokol@gmail.com>
2019-05-10 00:55:07 +0400

    sadgerdg

---
+++ quiz/build/precache-manifest.447aac5146ddaa3885b3cf3c86ed2134.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3068285a94e79392a7d0",
+    "url": "/quiz/static/js/runtime~main.3068285a.js"
+  },
+  {
+    "revision": "4f203afb39fc98fdc4ae",
+    "url": "/quiz/static/js/main.4f203afb.chunk.js"
+  },
+  {
+    "revision": "e1da162b12e52c2b5aed",
+    "url": "/quiz/static/js/1.e1da162b.chunk.js"
+  },
+  {
+    "revision": "4f203afb39fc98fdc4ae",
+    "url": "/quiz/static/css/main.87b3b193.chunk.css"
+  },
+  {
+    "revision": "8f45240aabcdb633f82fb3e4e4e95f26",
+    "url": "/quiz/index.html"
+  }
+];
