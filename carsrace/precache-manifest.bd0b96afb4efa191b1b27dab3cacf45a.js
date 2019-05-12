da0b22a4d30bb63aca75cd3c4ec8643d0fb099d9
jetreal <bystryi.sokol@gmail.com>
2019-05-13 00:10:15 +0400

    carsRace

---
+++ carsrace/precache-manifest.bd0b96afb4efa191b1b27dab3cacf45a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0579cddbdb6bc54b4ac0086a378f6988",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ccb112e84a8b37f7fa9",
+    "url": "/static/css/main.5b9fc7cf.chunk.css"
+  },
+  {
+    "revision": "efa5d7f4cd7343b1f07a",
+    "url": "/static/js/2.73cea778.chunk.js"
+  },
+  {
+    "revision": "4ccb112e84a8b37f7fa9",
+    "url": "/static/js/main.c71e7a81.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
