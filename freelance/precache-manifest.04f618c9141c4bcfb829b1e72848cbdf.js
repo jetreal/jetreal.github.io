ff0b1bed56eacbddc643fd830f3993c72590bd3c
jetreal <bystryi.sokol@gmail.com>
2019-06-22 17:37:00 +0400

    g

---
+++ freelance/precache-manifest.04f618c9141c4bcfb829b1e72848cbdf.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79d26e51025acc78ddf1e2f2c269e125",
+    "url": "./index.html"
+  },
+  {
+    "revision": "64fda4801ce570d27366",
+    "url": "./static/css/main.fdcbbc90.chunk.css"
+  },
+  {
+    "revision": "1837dd5433b96323b762",
+    "url": "./static/js/2.690d016a.chunk.js"
+  },
+  {
+    "revision": "64fda4801ce570d27366",
+    "url": "./static/js/main.07c54e5d.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "06eeeb81039e637219435498dd2b6871",
+    "url": "./static/media/1.06eeeb81.jpg"
+  },
+  {
+    "revision": "300e9b233bb49ca009b98708d62f4285",
+    "url": "./static/media/backLeft2.300e9b23.jpg"
+  },
+  {
+    "revision": "a981276463bed1c788d2bed3b2a6c8c7",
+    "url": "./static/media/backRight.a9812764.jpg"
+  },
+  {
+    "revision": "48bd3a9c02d25f4af28b3e7c857c0092",
+    "url": "./static/media/flow.48bd3a9c.jpg"
+  }
+]);
