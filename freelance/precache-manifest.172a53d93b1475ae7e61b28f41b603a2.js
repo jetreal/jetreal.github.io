6971e66ee6cc5e90acd7f1a552bba1874760b226
jetreal <bystryi.sokol@gmail.com>
2019-06-24 15:45:58 +0400

    y

---
+++ freelance/precache-manifest.172a53d93b1475ae7e61b28f41b603a2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79f9cf54efc0fa7c4c15ded951d92e55",
+    "url": "./index.html"
+  },
+  {
+    "revision": "056a323ea730c87d7d02",
+    "url": "./static/css/main.d750772d.chunk.css"
+  },
+  {
+    "revision": "1837dd5433b96323b762",
+    "url": "./static/js/2.690d016a.chunk.js"
+  },
+  {
+    "revision": "056a323ea730c87d7d02",
+    "url": "./static/js/main.1229fa49.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "300e9b233bb49ca009b98708d62f4285",
+    "url": "./static/media/backLeft2.300e9b23.jpg"
+  },
+  {
+    "revision": "a981276463bed1c788d2bed3b2a6c8c7",
+    "url": "./static/media/backRight.a9812764.jpg"
+  },
+  {
+    "revision": "48bd3a9c02d25f4af28b3e7c857c0092",
+    "url": "./static/media/flow.48bd3a9c.jpg"
+  }
+]);
