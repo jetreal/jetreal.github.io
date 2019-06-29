d21533053d04e9468202069f5ee30d6b7a79ca7c
jetreal <bystryi.sokol@gmail.com>
2019-06-29 16:39:12 +0400

    dt

--- social/service-worker.js
+++ social/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d9adfed6e2b62cc5ef649063d0815084.js"
+  "./precache-manifest.ad9cb8d6098d2e49a894551f6f53865d.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
