12763074445a8fbecebaf919e1a539b63ed08dd6
jetreal <bystryi.sokol@gmail.com>
2019-06-30 14:00:11 +0400

    webp

--- freelance/service-worker.js
+++ freelance/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.9990b0d426039443fa4cf43e8514f081.js"
+  "./precache-manifest.13ec98260a7ca1ab631e40e045ff0a1c.js"
 );
 
 self.addEventListener('message', (event) => {
