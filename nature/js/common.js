9b0429ed9d21848dab5143fbb2e4d9afac1b3d2d
jetreal <bystryi.sokol@gmail.com>
2019-07-14 09:51:15 +0400

    button fixed

--- nature/js/common.js
+++ nature/js/common.js
@@ -173,35 +173,36 @@ function film() {
 	}
 	// button toggle function 
 	function mDown() {
-		switch (true) {
-			case (flagi == false ) :
-				$('.circleImg').off('click').one('click', headerAnime);
-				$('.circleImg').css({width:'140px',height:'140px',
-					'box-shadow': '3px 3px 6px #19B9F5, -3px -3px 6px #19B9F5,\
+
+		if (flagi === false) {
+			$('.circleImg').off('click').one('click', headerAnime);
+			$('.circleImg').css({
+				width: '140px', height: '140px',
+				'box-shadow': '3px 3px 6px #19B9F5, -3px -3px 6px #19B9F5,\
 					-3px 3px 6px #19B9F5, 3px -3px 6px #19B9F5'});
-				// css
-				$('.circleImg').attr('src', 'img/images/button2.webp');
-				$('.wr17 .into03, .wr17 .into04, .wr17 .into05, .wr17 .into06')
+			// css
+			$('.circleImg').attr({ 'src': '/img/images/button2.webp' });
+			$('.wr17 .into03, .wr17 .into04, .wr17 .into05, .wr17 .into06')
 				.css('box-shadow', '3px 3px 6px #19B9F5 inset, -3px -3px 6px #19B9F5 inset');
-				$('.bDiv').css('border', '1px solid #19B9F5');
-				// change flag
-				flagi = true;
-			break;
-			case (flagi == true) :
+			$('.bDiv').css('border', '1px solid #19B9F5');
+			// change flag
+			flagi = true;
+			setTimeout(function () {
 				$('.circleImg').off('click');
 				$('.circleImg').one('click', offTimerHeader);
 				// css
-				$('.circleImg').attr('src', 'img/images/button3.webp');
-				$('.circleImg').css({width:'150px',height:'150px',
-				 'box-shadow': '3px 3px 6px gray, -3px -3px 6px gray,\
-				 -3px 3px 6px gray, 3px -3px 6px gray'});
+				$('.circleImg').attr('src', '/img/images/button3.webp');
+				$('.circleImg').css({
+					width: '150px', height: '150px',
+					'box-shadow': '3px 3px 6px gray, -3px -3px 6px gray,\
+					-3px 3px 6px gray, 3px -3px 6px gray'});
 				$('.wr17 .into03, .wr17 .into04, .wr17 .into05, .wr17 .into06')
-				.css('box-shadow', '3px 3px #7F7F7F inset, -3px -3px #7F7F7F inset');
+					.css('box-shadow', '3px 3px #7F7F7F inset, -3px -3px #7F7F7F inset');
 				$('.bDiv').css('border', '1px solid #99747A');
 				// change flag
 				flagi = false;
-			break;
-		}	
+			}, 32000)
+		}
 	}
 	// start function
 	$('.circleImg').on('mousedown', mDown);
