becac649eb00fad28b3eaba4061ba15a7d56d622
jetreal <41300107+jetreal@users.noreply.github.com>
2019-08-21 15:48:51 +0300

    fdag

--- menu/js/index.js
+++ menu/js/index.js
@@ -51,15 +51,15 @@ $(document).ready(function () {
   })
 })
 
-jQuery(function($){
-	$(document).mouseup(function (e){ // событие клика по веб-документу
-		var div = $("#mobileNav"); // тут указываем ID элемента
-		if (!div.is(e.target) // если клик был не по нашему блоку
-		    && div.has(e.target).length === 0) { // и не по его дочерним элементам
-          hideSidebar() // скрываем его
-		}
-	});
-});
+// jQuery(function($){
+// 	$(document).mouseup(function (e){ // событие клика по веб-документу
+// 		var div = $("#mobileNav"); // тут указываем ID элемента
+// 		if (!div.is(e.target) // если клик был не по нашему блоку
+// 		    && div.has(e.target).length === 0) { // и не по его дочерним элементам
+//           hideSidebar() // скрываем его
+// 		}
+// 	});
+// });
 
 
 
