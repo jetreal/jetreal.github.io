0c53f7f2a90e97b36129f8668492dae72d6fc0ad
Jetreal <bystryi.sokol@gmail.com>
2019-09-26 12:11:14 +0400

    freelance site added

---
+++ freelance/precache-manifest.4500c0ab171b6397a8f2cb5457d71826.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d9e2efbbc278d6c1428d6199341fc3f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9391f8de1f7dc2c45c89",
+    "url": "./static/css/main.60296f7f.chunk.css"
+  },
+  {
+    "revision": "e4d250a8df0ef43cb950",
+    "url": "./static/js/2.90ad88a1.chunk.js"
+  },
+  {
+    "revision": "9391f8de1f7dc2c45c89",
+    "url": "./static/js/main.ad02123d.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
