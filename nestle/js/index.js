dcd595fd0d14f55ba28b0fc0c930e0a516a16446
jetreal <41300107+jetreal@users.noreply.github.com>
2020-01-31 15:40:49 +0300

    progress line added

--- nestle/js/index.js
+++ nestle/js/index.js
@@ -1,4 +1,29 @@
+// horizontal progress function
+window.onload = function(){
+  var progressLine = document.getElementById("progress-line");
+  var body = document.body,
+      html = document.documentElement,
+      viewportHeight = window.innerHeight;
 
+  var documentHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
+  
+  var scrollTopValue = function(){
+    return (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+  }
+  
+  window.addEventListener("scroll", function(){
+    var scroll = scrollTopValue();
+    var progress = (scroll / (documentHeight - viewportHeight))*100;
+    progressLine.style.width = progress + "%";
+  });
+  
+  window.addEventListener("resize", function(){
+    body = document.body;
+    html = document.documentElement;
+    viewportHeight = window.innerHeight;
+    documentHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
+  });
+}
 /////////////////////////////////////////////////////////////////
 
 // init wow 
