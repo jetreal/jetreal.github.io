14ec9411cc6e134035213387755372c2da887276
jetreal <41300107+jetreal@users.noreply.github.com>
2020-02-28 20:10:48 +0300

    home page added

---
+++ paymentsystem/precache-manifest.bf8cb2a846ff00a9afe9c6e07bb2266f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "606960198f19b451478483445343e0b2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4d5ae1ae1fedcee508a7",
+    "url": "./static/css/main.afb3b4c2.chunk.css"
+  },
+  {
+    "revision": "82c6f68991118d08d492",
+    "url": "./static/js/2.281bd1ce.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "./static/js/2.281bd1ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d5ae1ae1fedcee508a7",
+    "url": "./static/js/main.b1d0a4ab.chunk.js"
+  },
+  {
+    "revision": "fc13c32f8c2bfd0cc08b",
+    "url": "./static/js/runtime-main.76606cd9.js"
+  }
+]);
--- paymentsystem/service-worker.js
+++ paymentsystem/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4a7fcba5ad5c86aeefed696e290fc72c.js"
+  "./precache-manifest.bf8cb2a846ff00a9afe9c6e07bb2266f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
