1933648e992ac540bc16371fa277d765adeb1e85
jetreal <41300107+jetreal@users.noreply.github.com>
2020-02-28 21:12:22 +0300

    try deploy again

--- paymentsystem/service-worker.js
+++ paymentsystem/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.bf8cb2a846ff00a9afe9c6e07bb2266f.js"
+  "/paymentsystem/precache-manifest.480cfa2251dcfa23583aa65d91168b12.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/paymentsystem/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
