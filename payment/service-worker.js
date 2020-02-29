ece3b3bf5a19aa718c89ac5ce2dfced99a48c066
jetreal <41300107+jetreal@users.noreply.github.com>
2020-02-29 09:32:34 +0300

    try again

--- payment/service-worker.js
+++ payment/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.bf8cb2a846ff00a9afe9c6e07bb2266f.js"
+  "./../precache-manifest.e8e9ccf572d42672a417f3db512e320e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./../index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
